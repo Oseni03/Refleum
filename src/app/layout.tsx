e06b90d46c17b6571bd2aea09e9bbcb7ae3d30b9
Oseni03 <oseniayomide57@gmail.com>
2026-05-17 02:04:38 +0100

    fix: upgrade Next.js version to fix CVE-2025-55182 vulnerability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { JetBrains_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { OrganizationStoreProvider } from "@/zustand/providers/organization-store-provider";
 import { siteConfig } from "@/config/site";
-import { cn } from "@/lib/utils";
 import { TooltipProvider } from "@/components/ui/tooltip";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const jetbrainsMono = JetBrains_Mono({
-    subsets: ["latin"],
-    variable: "--font-mono",
-    weight: ["400"],
-});
 
 export const metadata: Metadata = {
     title: siteConfig.name,
@@ -30,9 +20,9 @@ export default function RootLayout({
         <html
             lang="en"
             suppressHydrationWarning
-            className={cn(jetbrainsMono.variable, "font-sans", inter.variable)}
+            className="font-sans"
         >
-            <body className={`font-sans antialiased`}>
+            <body className="font-sans antialiased">
                 <TooltipProvider>
                     <ThemeProvider>
                         <OrganizationStoreProvider>
